d719135783e5184a35247aa2539160c544d2d7de
jlelis <jerfao@hotmail.com>
2021-07-05 17:32:50 -0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\ProductPhoto;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $product = Product::create($request->all());
+
+        if ($request->hasFile('photos'))
+        {
+            foreach($request->file('photos') as $file){
+                    // $name=$file->getClientOriginalName();
+                    $filename = $file->store('photos');
+                    // $file->move('storage/photos',$name);
+                    // $filename=time().'_'.$name;
+                    // dd($filename);
+                    ProductPhoto::create([
+                        'product_id'=>$product->id,
+                        'path_images'=>$filename,
+                    ]);
+            }
+        }
+        return redirect()->back()->with('message','salvo com sucesso!');
+    }
+
+    private function imageUpload($images, $imageColumn = null)
+	{
+
+		$uploadedImages = [];
+
+		if(is_array($images)) {
+
+			foreach($images as $image) {
+					$uploadedImages[] = [$imageColumn => $image->store('products', 'public')];
+			}
+
+		} else {
+			$uploadedImages = $images->store('logo', 'public');
+		}
+
+		return $uploadedImages;
+	}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade - Copia.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Jquery -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+     <!-- Compiled and minified JavaScript -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="col-lg-12">
+
+        <h1 class="my-4">New Product</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            Name:
+            <br />
+            <input type="text" name="name" value="{{ old('name') }}" class="form-control" />
+            <br />
+
+            Photo:
+            <br />
+            <input type="file" name="photos[]" multiple/>
+            <br /><br />
+
+            <input type="submit" class="btn btn-primary" value="Save" />
+            <br /><br />
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
